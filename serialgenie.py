6b6c18355e7a7729c5be6cde2bd23af1e74c5edc
Rémi Sarrailh <remi@usini.eu>
2023-01-23 12:08:26 +0100

    Add Disconnect when lose focus

--- serialgenie.py
+++ serialgenie.py
@@ -7,6 +7,8 @@ import threading
 #import sv_ttk
 from datetime import datetime
 import re
+import pyautogui
+import sys
 
 ANSI_COLOR = {}
 ANSI_COLOR["0;30"] = "black"
@@ -18,6 +20,18 @@ ANSI_COLOR["0;35"] = "magenta"
 ANSI_COLOR["0;36"] = "cyan"
 ANSI_COLOR["0;37"] = "white"
 
+ser = None
+connected = False
+dis_on_focus_out = False
+first_device = None
+
+"""
+windows = pyautogui.getAllWindows()
+for window in windows:
+    print(window.title)
+sys.exit(0)
+"""
+
 def get_first_device():
     ports = list(serial.tools.list_ports.comports())
     print("<------ PORTS ------->")
@@ -26,12 +40,13 @@ def get_first_device():
         if port.device != "COM1":
             return port.device
     return None
-ser = None
-connected = False
+
 
 def send_data():
     global ser, connected
     data = entry.get()
+    print("Data send: " + data)
+    data = data + "\r\n"
     if ser is not None:
         ser.write(data.encode())
 
@@ -74,9 +89,9 @@ def detect_esp32_boot(data):
     return True
 
 def receive_data():
-    global ser, connected
+    global ser, connected, dis_on_focus_out
     while True:
-        if connected:
+        if connected and dis_on_focus_out == False:
             try:
                 data = ser.readline().decode("utf-8")
             except:
@@ -93,7 +108,23 @@ def receive_data():
                     else:
                         text_widget.insert('end', data)
                     text_widget.see('end')
-           
+
+def on_focus_out(event):
+    global dis_on_focus_out, connected, first_device
+    if dis_on_focus_out == False and event.widget.master == None:
+        print("Focus Out disconnected")
+        ser.close()
+        root.title(first_device + " (Pause)")
+        dis_on_focus_out = True    
+        connected = False
+        text_widget.tag_config("reset", background="red", foreground="white")
+        text_widget.insert('end', "PAUSE\n", "reset")    
+
+def on_focus_in(event):
+    global dis_on_focus_out
+    if dis_on_focus_out == True:
+        print("Focus in reconnect")
+        dis_on_focus_out = False
 
 def on_disconnect(ser):
     global connected
@@ -103,30 +134,35 @@ def on_disconnect(ser):
 
 def reset():
     global ser
-    ser.setDTR(False)
-    time.sleep(0.022)
-    ser.setDTR(True)
+    if connected == True:
+        ser.setDTR(False)
+        time.sleep(0.022)
+        ser.setDTR(True)
+        text_widget.tag_config("reset", background="red", foreground="white")
+        text_widget.insert('end', "RESET\n", "reset")    
 
 def connection_manager():
-    global ser, connected
+    global ser, connected, dis_on_focus_out, first_device
     while True:
         first_device = get_first_device()
-        if connected is False:
+        if connected is False and dis_on_focus_out is False:
             if(first_device is not None):
+                print(dis_on_focus_out)
                 print("Connecting...")
                 try:
                     ser = None
                     ser = serial.Serial(first_device, 115200)
                     print("Connection succeed")
                     connected = True
+                    text_widget.tag_config("reset", background="red", foreground="white")
+                    text_widget.insert('end', "START\n", "reset")   
                 except:
-                    raise
                     print("Connection failed")
-                    root.title("Disconnected")
+                    root.title("Already in used")
                     connected = False
                 if connected:
                     root.title(first_device)
-                    reset()
+                    #reset()
             else:
                 print("No device founded")
                 root.title("No device")
@@ -148,8 +184,6 @@ text_frame.pack(expand=True, fill='both')
 
 text_widget = tk.Text(text_frame)
 
-
-
 text_widget.config(bg=bg_color, fg=fg_color)
 text_widget.pack(expand=True, side="left", fill='both')
 
@@ -163,8 +197,12 @@ entry_frame = tk.Frame(root)
 entry_frame.pack(fill='x')
 entry = ttk.Entry(entry_frame, width=20)
 entry.pack(side='left', fill='x', expand=True)
+
 send_button = ttk.Button(entry_frame, text="Send", command=send_data)
 send_button.pack(side='right')
+
+reset_button = ttk.Button(entry_frame, text="Reset", command=reset)
+reset_button.pack(side='right')
 
 receive_thread = threading.Thread(target=receive_data)
 receive_thread.start()
@@ -172,6 +210,9 @@ receive_thread.start()
 connection_thread = threading.Thread(target=connection_manager)
 connection_thread.start()
 
+
+root.bind("<FocusOut>", on_focus_out)
+root.bind("<FocusIn>", on_focus_in)
 root.attributes("-topmost", True)
 root.attributes("-alpha", 0.95)
 # sv_ttk.use_dark_theme()
